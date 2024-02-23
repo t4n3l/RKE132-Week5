fd9d7cba2ea1b388af289e0d9f246f6e431554c4
Tanel Ristsalu <tanel.ristsalu@tktk.ee>
2024-02-23 14:39:21 +0200

    Videotööd

--- Week5/Program.cs
+++ Week5/Program.cs
@@ -1,1 +1,14 @@
 ﻿
+
+//AND - &&
+//true && true --> true
+//true && false --> false
+//false && true --> false
+//false && false --> false
+
+//OR - ||
+
+//true || true --> true
+//true || false --> true
+//false || true --> true
+//false || false --> false
