42a491560132315f16776ebb6a475548d7830407
Tanel Ristsalu <tanel.ristsalu@tktk.ee>
2024-02-23 14:45:30 +0200

    Moodle test

---
+++ Moodle/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("Enter the first letter of your first name:");
+char userLetter = Char.Parse(Console.ReadLine().ToLower());
+
+if (userLetter == 'a' || userLetter == 'e' || userLetter == 'i' || userLetter == 'o' || userLetter == 'u')
+{
+    Console.WriteLine("I can sing it.");
+}
+else
+{
+    Console.WriteLine("There is no way I can sing it.");
+}
